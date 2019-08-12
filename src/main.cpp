7dd9018a6ad91160af88ce9985f47badddd6d626
Lucien Morey <lucienp@live.com.au>
2019-08-13 07:49:22 +1000

    created main function and set up libraries for include

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "jlmio.h"
+#include "LiquidCrystal.h"
 
-void setup() {
-  // put your setup code here, to run once:
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
+int main(){
+  LiquidCrystal lcd(8,9,4,5,6,7);
+
+  lcd.begin(16,2);
+
+  lcd.print("Hi");
+
+
+  while(1){
+
+  }
+
+  return 0;
 }
