f96e27e2e493db85d4f1e8bbdf6938ffb5dfc8a0
Lucien Morey <lucienp@live.com.au>
2019-08-14 23:10:08 +1000

    temp commit for going to school. completed constructor and step

--- src/StepperMotor.cpp
+++ src/StepperMotor.cpp
@@ -1,6 +1,31 @@
 #include "StepperMotor.h"
 
-StepperMotor::StepperMotor(){
+StepperMotor::StepperMotor(volatile uint8_t* dataDirection1, volatile uint8_t* pin1, uint8_t position1, volatile uint8_t* dataDirection2, 
+        volatile uint8_t* pin2, uint8_t position2, volatile uint8_t* dataDirection3, volatile uint8_t* pin3, uint8_t position3, 
+        volatile uint8_t* dataDirection4, volatile uint8_t* pin4, uint8_t position4){
+    
+    this->dataDirection1 = dataDirection1;
+    this->position1 = position1;
+    this->dataDirection2 = dataDirection2;
+    this->pin2 = pin2;
+    this->position2 = position2;
+    this->dataDirection3 = dataDirection3;
+    this->pin3 = pin3;
+    this->position3 = position3;
+    this->dataDirection4 = dataDirection4;
+    this->pin4 = pin4;
+    this->position4 = position4;
+
+    jlm::PinMode(this->dataDirection1,this->position1,OUTPUT);
+    jlm::PinMode(this->dataDirection2,this->position2,OUTPUT);
+    jlm::PinMode(this->dataDirection3,this->position3,OUTPUT);
+    jlm::PinMode(this->dataDirection4,this->position4,OUTPUT);
+
+    jlm::WritePin(this->pin1,this->position1,LOW);
+    jlm::WritePin(this->pin2,this->position2,LOW);
+    jlm::WritePin(this->pin3,this->position3,LOW);
+    jlm::WritePin(this->pin4,this->position4,LOW);
+
 
 }
 
@@ -8,8 +33,36 @@ StepperMotor::~StepperMotor(){
 
 }
 
-void StepperMotor::setStep(){
-
+void StepperMotor::Step(int stepState){
+    switch (stepState){
+    case 1:
+        jlm::WritePin(this->pin1,this->position1,HIGH);
+        jlm::WritePin(this->pin2,this->position2,LOW);
+        jlm::WritePin(this->pin3,this->position3,LOW);
+        jlm::WritePin(this->pin4,this->position4,LOW);
+        break;
+    
+    case 2:
+        jlm::WritePin(this->pin1,this->position1,LOW);
+        jlm::WritePin(this->pin2,this->position2,HIGH);
+        jlm::WritePin(this->pin3,this->position3,LOW);
+        jlm::WritePin(this->pin4,this->position4,LOW);
+        break;
+    
+    case 3:
+        jlm::WritePin(this->pin1,this->position1,LOW);
+        jlm::WritePin(this->pin2,this->position2,LOW);
+        jlm::WritePin(this->pin3,this->position3,HIGH);
+        jlm::WritePin(this->pin4,this->position4,LOW);
+        break;
+    
+    case 4:
+        jlm::WritePin(this->pin1,this->position1,LOW);
+        jlm::WritePin(this->pin2,this->position2,LOW);
+        jlm::WritePin(this->pin3,this->position3,LOW);
+        jlm::WritePin(this->pin4,this->position4,HIGH);
+        break;
+    }
 }
 
 void StepperMotor::setSpeed(){
@@ -17,5 +70,5 @@ void StepperMotor::setSpeed(){
 }
 
 void StepperMotor::setDirection(){
-    
+
 }
--- src/StepperMotor.h
+++ src/StepperMotor.h
@@ -1,18 +1,37 @@
 #ifndef __STEPPERMOTOR_H__
 #define __STEPPERMOTOR_H__
 
+#include "jlmio.h"
+
 class StepperMotor{
 
     public:
-        StepperMotor();
+        StepperMotor(volatile uint8_t* dataDirection1, volatile uint8_t* pin1, uint8_t position1, volatile uint8_t* dataDirection2, 
+            volatile uint8_t* pin2, uint8_t position2, volatile uint8_t* dataDirection3, volatile uint8_t* pin3, uint8_t position3, 
+            volatile uint8_t* dataDirection4, volatile uint8_t* pin4, uint8_t position4);
         ~StepperMotor();
-        void setStep();
+        void Step(int stepState);
         void setSpeed();
         void setDirection();
     private:
+        
         int direction;
         int speed;
         int Steps;
+        int stepState;
+
+        volatile uint8_t* dataDirection1;
+        volatile uint8_t* pin1;
+        uint8_t position1;
+        volatile uint8_t* dataDirection2;
+        volatile uint8_t* pin2;
+        uint8_t position2;
+        volatile uint8_t* dataDirection3;
+        volatile uint8_t* pin3;
+        uint8_t position3;
+        volatile uint8_t* dataDirection4;
+        volatile uint8_t* pin4;
+        uint8_t position4;
 
 };
 
