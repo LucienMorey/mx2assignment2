7c1f743aa17fcfe68eaab13182c179b6db6cd046
Lucien Morey <lucienp@live.com.au>
2019-08-22 06:56:42 +1000

    completed functionality pending marking

--- src/StepperMotor.h
+++ src/StepperMotor.h
@@ -6,19 +6,29 @@
 class StepperMotor{
 
     public:
-        StepperMotor(volatile uint8_t* dataDirection1, volatile uint8_t* pin1, uint8_t position1, volatile uint8_t* dataDirection2, 
+        StepperMotor(int stepsPerRev, volatile uint8_t* dataDirection1, volatile uint8_t* pin1, uint8_t position1, volatile uint8_t* dataDirection2, 
             volatile uint8_t* pin2, uint8_t position2, volatile uint8_t* dataDirection3, volatile uint8_t* pin3, uint8_t position3, 
             volatile uint8_t* dataDirection4, volatile uint8_t* pin4, uint8_t position4);
         ~StepperMotor();
-        void Step(int stepState);
-        void setSpeed();
-        void setDirection();
+        void step();
+        void setSpeed(int rpm);
+        void setDirection(int directionToSet);
+        int getDirection();
+
+        bool stepping;
+        volatile int steps;
+        float stepPeriod;
+        int rpm;
+        volatile long stepsTaken;
+        int stepsPerRev;
+
+        
+
     private:
         
         int direction;
-        int speed;
-        int Steps;
-        int stepState;
+        
+        volatile int stepState;
 
         volatile uint8_t* dataDirection1;
         volatile uint8_t* pin1;
@@ -32,6 +42,10 @@ class StepperMotor{
         volatile uint8_t* dataDirection4;
         volatile uint8_t* pin4;
         uint8_t position4;
+
+        
+
+        
 
 };
 
